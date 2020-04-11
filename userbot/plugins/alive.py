788eb27e2a078799c639d71f7b1d1274e28c8bbc
TEAMJACKE <62723001+TEAMJACKE@users.noreply.github.com>
2020-04-11 22:02:22 +0300

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -16,4 +16,22 @@ async def amireallyalive(alive):
                      "`Bot created by:` [SnapDragon](tg://user?id=719877937)\n"
                      "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
                      f"`My peru owner`: {DEFAULTUSER}\n"
-                     "[Deploy this userbot Now](https://github.com/Dark-Princ3/X-tra-Telegram)")
+          """Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""  
+import asyncio  
+from telethon import events  
+from telethon.tl.types import ChannelParticipantsAdmins  
+from platform import uname  
+from userbot import ALIVE_NAME  
+from userbot.utils import admin_cmd  
+  
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "No name set yet nibba, check pinned in @XtraTgBot"  
+  
+@command(outgoing=True, pattern="^.alive$")  
+async def amireallyalive(alive):  
+    """ For .alive command, check if the bot is running.  """  
+    await alive.edit("**`YO BRO JABTAK YE KHEL KHATAM NHI HOTA APUN IDHARICH HAI ψ(｀∇´)ψ`**\n\n"  
+                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @cuuuuuu\n"  
+                     "`Bot created by:` [𓆩 hassanmuaed 𓆪](tg://user?id=665877797)\n"  
+                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"  
+                     f"`My peru owner`: {DEFAULTUSER}\n"  
+                    "[لتنصيب البوت راسلني](t.me/hassanmuaed)")
